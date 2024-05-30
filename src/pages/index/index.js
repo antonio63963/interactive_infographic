78ee865f7cba348f70954d9ced281550bcccefc5
antonio63963 <shotvideo25@gmail.com>
2024-05-30 19:29:52 +0300

    fixed bug with chart animation apear the popup

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,1 +1,23 @@
 import '../../common/styles/index.css';
+import './index.css';
+import '../../common/ui/popupPage/popupPage.css'
+
+//Math Grants
+import '../mathGrants/mathGrants.css';
+import '../mathGrants/mediaMathGrants.css';
+import '../mathGrants/mathGrants.js';
+
+import {Chart} from 'chart.js/auto';
+
+
+function displayPopup(popup) {
+  popup.classList.remove('hidden');
+}
+function hidePopup(popup) {
+  popup.classList.add('hidden');
+}
+
+export {
+  displayPopup,
+  hidePopup,
+}
--- src/pages/mathGrants/mathGrants.js
+++ src/pages/mathGrants/mathGrants.js
@@ -3,6 +3,7 @@ import "../../common/ui/twoValuesRow/twoValuesRow.css";
 import "./mathGrants.css";
 import "./mediaMathGrants.css";
 import Chart from "chart.js/auto";
+import { displayPopup, hidePopup } from "../index";
 
 const canvas = document.querySelector("#grant-chart");
 const mathGrants = {
@@ -17,28 +18,51 @@ const mathGrants = {
   ],
 };
 
-new Chart(canvas, {
+const options = {
   type: "doughnut",
   data: {
     // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
     datasets: [
       {
         label: "Beneficiaries",
+        borderColor: "#ffffff",
         data: mathGrants.values,
         backgroundColor: mathGrants.valuesColor,
-        innerRadius: 10,
-        hoverOffset: 4,
+        hoverOffset: 6,
       },
     ],
-    // responsive: true,
-    // maintainAspectRatio: false,
 
     scales: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
+      y: {
+        beginAtZero: true,
       },
     },
   },
-});
+  options: {
+    cutout: 80,
+    borderColor: "#ffffff",
+
+  },
+};
+
+let mathBineficChart;
+function initMathGrantChart() {
+  const t = setTimeout(() => {
+    mathBineficChart = new Chart(canvas, options);
+  }, 500);
+}
+
+const toMathGrant = document.querySelector(".toMathGrant");
+const mathGrantClose = document.querySelector(".mathGrantClose");
+const mathGrantPage = document.querySelector(".mathGrantPage");
+
+function showMathGrantPage(e) {
+  displayPopup(mathGrantPage);
+  initMathGrantChart()
+}
+function closeMathGrantPage() {
+  hidePopup(mathGrantPage);
+  mathBineficChart.destroy();
+}
+toMathGrant.addEventListener("click", showMathGrantPage);
+mathGrantClose.addEventListener("click", closeMathGrantPage);
