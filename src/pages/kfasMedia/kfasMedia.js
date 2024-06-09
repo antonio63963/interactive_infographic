40e2066bd735face666544d2effe780fc2caa608
antonio63963 <shotvideo25@gmail.com>
2024-06-09 12:24:15 +0300

    custom bg in appTab on active

--- src/pages/kfasMedia/kfasMedia.js
+++ src/pages/kfasMedia/kfasMedia.js
@@ -20,24 +20,40 @@ kfasMediaClose.addEventListener("click", closeMathGrantPage);
 //animation elements
 
 const tabs = document.querySelectorAll(".kfasMediaPage .app-tab");
-const canvas = document.querySelector("#socials-chart").getContext('2d');
+const canvas = document.querySelector("#socials-chart").getContext("2d");
+let selectedTabIdx = 0;
 let socialsChart;
 
+function activateTab(selectedTab) {
+  const activeColor = selectedTab.dataset.color;
+  selectedTab.style.backgroundColor = activeColor;
+  selectedTab.classList.add("active");
+}
+function desactivateTab(unselectedTab) {
+  if (unselectedTab.classList.contains("active")) {
+     unselectedTab.style.removeProperty('background-color');
+    unselectedTab.classList.remove("active");
+  } else {
+    return;
+  }
+}
+
 function onTab(e) {
-  console.log(e.target.closest('.app-tab'), "TAB")
-  const tab = e.target.closest('.app-tab');
-  console.log('ID: ', tab.attributes.id.value)
+  console.log(e.target.closest(".app-tab"), "TAB");
+  const tab = e.target.closest(".app-tab");
+  console.log("ID: ", tab.attributes.id.value);
   const tabId = tab.attributes.id.value;
-  tabs.forEach(t => t.id == tabId ? t.classList.add('active') : t.classList.remove('active'))
+  tabs.forEach((t) => (t.id == tabId ? activateTab(t) : desactivateTab(t)));
 }
 
 function initTabs() {
-  tabs.forEach(t => t.addEventListener('click', onTab));
-  // const t = setTimeout(() => {
+  activateTab(tabs[0]);
+  tabs.forEach((t) => t.addEventListener("click", onTab));
+  const t = setTimeout(() => {
     socialsChart = initSocialChart(canvas, []);
-    
-  //   clearTimeout(t);
-  // }, 500);
+
+    clearTimeout(t);
+  }, 500);
 }
 
 // window.addEventListener('beforeprint', () => {
@@ -45,6 +61,6 @@ function initTabs() {
 // })
 
 function closeTabs() {
-  tabs.forEach(t => t.removeEventListener('click', onTab));
+  tabs.forEach((t) => t.removeEventListener("click", onTab));
   socialsChart.destroy();
 }
