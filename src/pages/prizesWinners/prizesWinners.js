f0e1d8b565628a3dc4796b5e54bf278f7e5f8756
antonio63963 <shotvideo25@gmail.com>
2024-06-13 17:11:17 +0300

    prize winners scrolled

--- src/pages/prizesWinners/prizesWinners.js
+++ src/pages/prizesWinners/prizesWinners.js
@@ -5,11 +5,19 @@ const toPrizeWinners = document.querySelector(".toPrizeWinners");
 const prizesWinnersPage = document.querySelector(".prizesWinnersPage");
 const prizesWinnersClose = document.querySelector(".prizesWinnersClose");
 
+function onOpacity(e) {
+  if (e.target.classList.contains("click-opacity")) {
+    closePrizeWinnersPage();
+  }
+}
+
 function showPrizeWinnersPage(e) {
   displayPopup(prizesWinnersPage);
+  prizesWinnersPage.addEventListener("click", onOpacity);
 }
 function closePrizeWinnersPage() {
   hidePopup(prizesWinnersPage);
+  prizesWinnersPage.removeEventListener("click", onOpacity);
 }
 
 toPrizeWinners.addEventListener("click", showPrizeWinnersPage);
