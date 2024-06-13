28febe19585c6bff00861de0935c37bfb0c877dc
antonio63963 <shotvideo25@gmail.com>
2024-06-13 16:53:02 +0300

    research capacity scrolled

--- src/pages/researchCapacity/researchCapacity.js
+++ src/pages/researchCapacity/researchCapacity.js
@@ -7,20 +7,22 @@ const toResearchCapacity = document.querySelector(".toResearchCapacity");
 const researchCapacityClose = document.querySelector(".researchCapacityClose");
 console.log(researchCapacityClose);
 const researchCapacityPage = document.querySelector(".researchCapacityPage");
-let isMobile = false;
+
+function onOpacity(e) {
+  if (e.target.classList.contains("click-opacity")) {
+    closeResearchCapacityPage();
+  }
+}
 
 function showResearchCapacityPage(e) {
   displayPopup(researchCapacityPage);
-
   animationResearchCapacity();
+  researchCapacityPage.addEventListener("click", onOpacity);
 }
 function closeResearchCapacityPage() {
   reverseAnimation();
   hidePopup(researchCapacityPage);
-  // const t = setTimeout(() => {
-  //   clearTimeout(t);
-  // }, 500);
-  // mathBineficChart.destroy();
+  researchCapacityPage.removeEventListener("click", onOpacity);
 }
 toResearchCapacity.addEventListener("click", showResearchCapacityPage);
 researchCapacityClose.addEventListener("click", closeResearchCapacityPage);
@@ -30,10 +32,9 @@ researchCapacityClose.addEventListener("click", closeResearchCapacityPage);
 const sidebarIcons = document.querySelectorAll(
   ".researchCapacityPage .sidebar-horizontal-item"
 );
-const capacityChartItems = 
-  document.querySelectorAll(".research-capacity-content .chart-item")
-;
-
+const capacityChartItems = document.querySelectorAll(
+  ".research-capacity-content .chart-item"
+);
 const theBigestFirstNum = Array.from(capacityChartItems)
   .map((item) => parseInt(item.dataset.val1, 10))
   .sort((a, b) => a > b)[0];
@@ -54,17 +55,16 @@ function animationResearchCapacity() {
     const val1 = item.dataset.val1;
     const val2 = item.dataset.val2;
 
-    const firstBar = item.querySelector('.first-bar');
-    const secondBar = item.querySelector('.second-bar');
+    const firstBar = item.querySelector(".first-bar");
+    const secondBar = item.querySelector(".second-bar");
 
-    const firstResult = firstBar.querySelector('.first-result');
-    const secondResult = firstBar.querySelector('.second-result');
+    const firstResult = firstBar.querySelector(".first-result");
+    const secondResult = firstBar.querySelector(".second-result");
 
-
-const t = setTimeout(() => {
+    const t = setTimeout(() => {
       firstBar.style.width = `${val1 * percent}%`;
       secondBar.style.width = `${val2 * percent}%`;
-      
+
       animatedCounter({
         elem: firstResult,
         numFrom: 0,
@@ -91,7 +91,7 @@ function reverseAnimation() {
     item.classList.add("moveToRight");
   });
   capacityChartItems.forEach((item, idx) => {
-    item.querySelector('.first-bar').style.width = 0;
-    item.querySelector('.second-bar').style.width = 0;
-  })
+    item.querySelector(".first-bar").style.width = 0;
+    item.querySelector(".second-bar").style.width = 0;
+  });
 }
